f38bb638d39def70f8771f77135824ce5f3bf02b
rene-romero <rene.romero.job@gmail.com>
2022-11-08 16:18:50 -0600

    gcp-3

--- gcp/data_transformation.py
+++ gcp/data_transformation.py
@@ -2,6 +2,7 @@ import argparse
 import time
 import logging
 import csv
+import re
 import apache_beam as beam
 from apache_beam.options.pipeline_options import GoogleCloudOptions
 from apache_beam.options.pipeline_options import PipelineOptions
@@ -35,6 +36,37 @@ def run():
     # Static input and output
     input = opts.input
     output = opts.output
+
+    def parse_method(string_input):
+    # This method translates a single line of comma separated values to a dictionary 
+    # which can be loaded into BigQuery.
+    # Args:
+    # string_input: A comma separated list of values in the form: 'Trip_Id, Trip__Duration,
+    # Start_Station_Id, Start_Time, Start_Station_Name, End_Station_Id, End_Time, 
+    # End_Station_Name, Bike_Id, User_Type'
+    # Example string_input: '10000083,720,7239,2020-03-10 13:28:00,
+    # Bloor St W / Manning Ave - SMART,7160, 10/03/2020 13:40,
+    # King St W / Tecumseth St,5563,Annual Member'
+    # Returns:
+    # A dict mapping BigQuery column names as keys to the corresponding value
+    # parsed from string_input.
+    # Example output:
+    #     {'Trip_Id':'10000083',
+    #     'Trip__Duration':'720',
+    #     'Start_Station_Id':'7239',
+    #     'Start_Time':'2020-03-10 13:28:00',
+    #     'Start_Station_Name':'Bloor St W / Manning Ave - SMART',
+    #     'End_Station_Id':'7160',
+    #     'End_Time':'2020-10-03 13:40:00',
+    #     'End_Station_Name':'King St W / Tecumseth St',
+    #     'Bike_Id':'5563',
+    #     'User_Type':'Annual Member'}
+    
+    # Strip out carriage return, newline and quote characters.
+        values = re.split(",", re.sub('\r\n', '', re.sub('"', '',string_input)))
+        row = dict(
+            zip(('year','followers','is_business_account','post_type','number_characters','number_hashtags','total_likes','total_comments'),values))
+        return row
 
     # Table schema for BigQuery
     table_schema = {
@@ -87,9 +119,9 @@ def run():
     '''
 
     (p
-        | 'ReadFromGCS' >> beam.io.ReadFromText(input, skip_header_lines=1)
-        | 'ParseJson' >> beam.Map(lambda line: csv.loads(line))
-        | 'WriteToBQ' >> beam.io.WriteToBigQuery(
+        | 'Read_from_GCS' >> beam.io.ReadFromText(input, skip_header_lines=1)
+        | 'Data_Transformation' >> beam.Map(parse_method)
+        | 'Write_to_BigQuery' >> beam.io.WriteToBigQuery(
             output,
             schema=table_schema,
             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
